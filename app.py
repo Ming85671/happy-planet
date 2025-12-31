07a2bead43bf7ae030f6c1430c7dfbd847f185ba
Ming <a58475@Ming.local>
2026-01-01 00:11:36 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+ 
+st.markdown(
+   """
+<style>
+   .title { font-size: 100px; color: red; text-align: center; }
+   .text { font-size: 40px; color: blue; text-align: center; }
+</style>
+<div class="title">快乐星球 网站</div>
+<div class="text">欢迎 Yiwen 来到 快乐星球 网站！</div>
+<div class="text">祝 Yiwen 元旦快乐，2026财源滚滚！</div>
+   """,
+   unsafe_allow_html=True
+)
